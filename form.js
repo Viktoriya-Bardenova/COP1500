7d364f38fd21c5a51b087b91011b50ef0bf6a5f8
Viktoriya Bardenova <47703694+Viktoriya-Bardenova@users.noreply.github.com>
2019-04-27 12:23:26 -0400

    Create form.js

---
+++ form.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Form</title> <!--CSS file here -->
+<link href="css/form.css" rel="stylesheet">
+</head>
+<body>
+<div id="main">
+<h1>Contact Form using JavaScript</h1>
+<div id="form_sample"></div> <!-- JS file here -->
+<script src="js/form.js"></script>
+</div>
+</body>
+</html>
